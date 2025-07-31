b1a8b64caec205ef48f4e05896d8158484b3bb46
thnhtus <fallforthat@gmail.com>
2025-07-31 04:21:34 +0000

    Sync LeetCode submission Runtime - 462 ms (22.61%), Memory - 0.0B (100.00%)

--- synced-problems/1258-article-views-i/solution.sql
+++ synced-problems/1258-article-views-i/solution.sql
@@ -1,6 +1,2 @@
--- Write your PostgreSQL query statement below
-select author_id as id from Views v
-    where v.viewer_id = v.author_id
-    group by id
-    order by id asc
-
+# Write your MySQL query statement below
+select distinct v.author_id as id from Views as v where v.author_id = v.viewer_id order by v.author_id asc
