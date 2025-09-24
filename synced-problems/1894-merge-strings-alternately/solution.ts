44173b6613156a185476568eccaf4a2d1a774da7
thnhtus <fallforthat@gmail.com>
2025-09-24 15:43:47 +0000

    Sync LeetCode submission Runtime - 45 ms (58.04%), Memory - 55.2 MB (80.97%)

---
+++ synced-problems/1894-merge-strings-alternately/solution.ts
@@ -0,0 +1,12 @@
+function mergeAlternately(word1: string, word2: string): string {
+    let result = '';
+
+    const max = Math.max(word1.length, word2.length);
+
+    for (let i = 0; i < max; i++) {
+        result += word1[i] || '';
+        result += word2[i] || '';
+    }
+
+    return result;
+}
